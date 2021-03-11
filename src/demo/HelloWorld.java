d087cda15ae98f24de9d420b2b35ef80faba55ea
HEINKHANTWIN <63330586+HEINKHANTWIN@users.noreply.github.com>
2021-03-12 01:51:12 +0800

    3rd commit

--- src/demo/HelloWorld.java
+++ src/demo/HelloWorld.java
@@ -3,7 +3,7 @@ package demo;
 public class HelloWorld {
     public static void main(String[] args) {
     	
-        System.out.println("Hello World2!");
+        System.out.println("Hello World3!");
         
         ExchangeRateCoverter erc = new ExchangeRateCoverter(1.34);
         double g = erc.covertCurrency(1000);
